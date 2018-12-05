015f14678d2eaeec589087fa0c4a65726c3a9134
Edwin Rivera <edwin.rivera52102@gmail.com>
2018-12-05 22:44:43 +0000

    completed

--- js/script.js
+++ js/script.js
@@ -11,8 +11,52 @@
 $(function () {
    // now we are ready!
    $("#shoot").click(function(){
-       $("#userChoice").text($("#input").val());
-   
-       
+      var computerChoice = "";
+      var x = Math.random();
+      var UC= $("#input").val();
+      if (x <= 0.33){
+         computerChoice="rock";
+      }
+      else if(x <= 0.67) {
+         computerChoice="paper";
+      }
+      else {
+         computerChoice="scissors";
+      }
+      
+      var result = "";
+      
+      if(UC==computerChoice){
+         result = "TIE";
+      }
+      else if(UC=="rock"&&computerChoice=="paper"){
+         result="LOSER";
+      }
+      else if(UC=="paper"&&computerChoice=="scissors"){
+         result="LOSER";
+      }
+      else if(UC=="scissors"&&computerChoice=="rock"){
+         result="LOSER";
+      }
+      else if(UC=="rock"&&computerChoice=="scissors"){
+         result="YOU WIN";
+      }
+      else if(UC=="scissors"&&computerChoice=="scissors"){
+         result="YOU WIN";
+      }
+      else if(UC=="paper"&&computerChoice=="rock"){
+         result="YOU WIN";
+      }
+      else{
+         alert("ERROR");
+      }
+      
+      
+      
+      
+       $("#userChoice").text(UC);
+       $("#computerChoice").text(computerChoice);
+       $("#result").text(result);
    });
+    
 });
